10ec200c13cdb6647ef8ef900fdb7d0349394c06
CloudAppsDeveloper <developer.cloudapps@gmail.com>
2021-11-07 11:08:41 +0530

    initial commit
    initial commit

---
+++ src/main/java/com/cloudapps/developer/AwsSpringbootSnsApplication.java
@@ -0,0 +1,13 @@
+package com.cloudapps.developer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSpringbootSnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSpringbootSnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudapps/developer/AwsSpringbootSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudapps.developer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSpringbootSnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
